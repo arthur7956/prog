004f486092002d63f15a53781c67462e4497682f
arthur7956 <arthur.souza.silva14@escola.pr.gov.br>
2026-08-04 14:32:53 +0000

    ex 03 feito

---
+++ listas/lista2/ex03-C.c
@@ -0,0 +1,32 @@
+//c) Palindromo. int ehPalindromo(char s[], int ini, int fim) que retorna 1 se a pala-
+//vra e um palindromo (ex.: "arara"), e 0 caso contrario. Considere a string ja em minusculas e
+//sem espacos.
+#include <stdio.h>
+int ehPalindromo (char s[], int ini, int fim) {
+    if (ini >= fim) {
+        return 1;
+    }
+    if (s[ini] != s[fim]) {
+        return 0;
+    }
+    return ehPalindromo(s, ini + 1, fim - 1);
+
+}
+int main() {
+    char palavra[] = "arara";
+
+    int tamanho = 0;
+
+    while (palavra[tamanho] != '\0') {
+        tamanho++;
+    }
+
+    int fim = tamanho - 1;
+
+    if (ehPalindromo (palavra, 0, fim)) {
+        printf("A palavra '%s' eh palindromo", palavra);
+    } else {
+        printf("A palavra '%s' nao eh palindromo", palavra);
+    }
+    return 0;
+}
